4342ba11dcd1942550d4d63cc0bd96d69d16f070
NikhilKapade <nikhilkapade29@gmail.com>
2026-03-19 11:35:57 +0530

    role created to access s3 and EC2

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ resource "aws_instance" "server" {
   ami = "ami-0b0b78dcacbab728f"
   instance_type = var.my_EC2_INSTANCE
   key_name="terraform1"
- vpc_security_group_ids = ["sg-0ade33fcd0f4bf4a4"]
- 
- tags = {
+  vpc_security_group_ids = ["sg-0ade33fcd0f4bf4a4"]
+  iam_instance_profile = "EC2_s3_terraform"
+
+  tags = {
     Name= "Terra1"
  }
 
