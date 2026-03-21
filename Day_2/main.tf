4d800d3155d680b9f611041d62656ad31efb15c6
NikhilKapade <nikhilkapade29@gmail.com>
2026-03-21 14:12:50 +0530

    ok

--- Day_2/main.tf
+++ Day_2/main.tf
@@ -1,8 +1,8 @@
 module "ec2" {
-  source = "./modules/ec2"
+  source = "../modules/ec2"
   ami_id = "ami-058f0cf1cc0bc26b3"
   instance_type = "t3.micro"
-  name = "ec2-instance"
+  name = null
 }
 
 output "ec2_id" {
